8b2552a6f6cfbd08694801331c305aee59f1816d
raju790126 <venkatapadmarajubhusarapu123@gmail.com>
2026-01-06 03:06:24 +0530

    newlines

--- squareAndCube.java
+++ squareAndCube.java
@@ -1,20 +1,20 @@
 package codepractice;
-
+//find square and cube value for given integer...
 import java.util.Scanner;
 
 public class squareAndCube {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);  //Scanner method for dynamic input
 		System.out.println("Enter Number");
-		int n = scan.nextInt();
-		System.out.println("Type -> sqaure for S or Cube for C");
-		String name = scan.next();
-		if(name.equals("S")) {
-			square(n);
+		int n = scan.nextInt(); //scan the input and assign to variable
+		System.out.println("Type -> sqaure for S or Cube for C"); //Asking user for square or cube of the number.
+		String name = scan.next(); //scan the letter for calculation
+		if(name.equals("S")) { //checking the letter
+			square(n);         // calling method for square
 		}else if (name.equals("C")) {
-			cube(n);
+			cube(n);           //calling method for cube
 		}else {
 			System.out.println("Re-Type the letters in correct manner...");
 		}
@@ -23,13 +23,13 @@ public class squareAndCube {
 	}
 
 	public static void square(int n) {
-		// TODO Auto-generated method stub
-		System.out.println(n*n);
+		// TODO Auto-generated method stub --> square of a number
+		System.out.println(n*n); //print the square of the number
 		
 	}
 	public static void cube(int n) {
-		// TODO Auto-generated method stub
-		System.out.println(n*n*n);
+		// TODO Auto-generated method stub --> Cube of a number
+		System.out.println(n*n*n); //print the cube of the number
 		
 	}
 
